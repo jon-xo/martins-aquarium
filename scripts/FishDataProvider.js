fd7c5ffab398f8dedc4e578560e5424ca0b2e40f
jon-xo <jonn@hey.com>
2021-01-11 11:07:35 -0500

    Created FishDataProvider.js with an array of objects to build a library of fish. Updated index.html with link to javascript files.

---
+++ scripts/FishDataProvider.js
@@ -0,0 +1,39 @@
+/*
+ *  To get you started, here's some properties of Bart.
+ *  You need to add more properties to complete his
+ *  representation as an object. Then add all the other
+ *  fish to the collection.
+ */
+
+  const fishCollection = [
+      {
+          name: "George",
+          species: "Beta Fish",
+          length: "2 inches",
+          locationFound: "Galapagos Island",
+          Food: "Pretzels"
+      },
+      {
+        name: "Frank",
+        species: "Gold Fish",
+        length: "3 inches",
+        locationFound: "Barbadis",
+        Food: "Snails"
+      },
+      {
+        name: "Mike",
+        species: "Sword Fish",
+        length: "4 feet, 6 inches",
+        locationFound: "Virgin Islands",
+        Food: "Clown Fish"
+      },
+      {
+        name: "Samantha",
+        species: "Clown Fish",
+        length: "3 inches",
+        locationFound: "Hawaii",
+        Food: "Plankton"
+      },
+  ]
+
+  console.log(fishCollection);
